24517a88d065929333ac8470e08391f3ed1d4fc4
Suyash Verma <suyashverma@Suyashs-MacBook-Air.local>
2026-09-17 00:36:18 +0530

    added logging file

---
+++ Easy Pipelines/csv_pandas_postgres/src/utils.py
@@ -0,0 +1,11 @@
+import logging
+
+def get_logger_func(name):
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    return get_logger_func(name)
+
+
+
